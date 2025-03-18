62d9e6292e87f154b123e05c94737d84d335f40a
terrence-ou <outerrencedl@gmail.com>
2025-03-18 15:16:36 -0400

    .gitignore updated

---
+++ AddressList/AddressList/AddressListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressListApp.swift
+//  AddressList
+//
+//  Created by Tingsong Ou on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AddressListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
